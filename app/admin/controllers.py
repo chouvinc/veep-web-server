1f3248bf4cc8ec2901d6f2e4509c5093e50d762e
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-07-21 19:09:03 -0400

    Added footer content, added log-in dependency flask_login, added an endpoint to admin

--- app/admin/controllers.py
+++ app/admin/controllers.py
@@ -1,9 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_login import current_user
 
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
-
+@admin.route('/admin')
 @admin.route('/')
 def index():
-    return "Admin"
+    if current_user.is_authenticated:
+        return render_template("submit.htm")
+    else:
+        return render_template("login.htm")
+
+
+@admin.route('/submit', methods=['POST'])
+@admin.route('/submit/<table>')
+def submit(table):
+    form_data = request.form
--- app/main/controllers.py
+++ app/main/controllers.py
@@ -14,7 +14,29 @@ def index():
         # {"title": "Contact Us", "url": url_for('.contact_us')}
     ]
 
-    return render_template("index.htm", links=links)
+    footer_items = [
+        {"title": "Social Media",
+            "items": [
+                # TODO figure out what social media we have
+                {"title": "Facebook", "url": "/"},
+                {"title": "Placeholder", "url": "/"}
+            ]
+        },
+        {"title": "Apply",
+            "items": [
+                # TODO figure out which exec positions ppl can apply to
+                {"title": "Project Manager", "url": "/"},
+                {"title": "Digital Team", "url": "/"},
+                {"title": "Marketing", "url": "/"},
+                {"title": "Operations", "url": "/"},
+                {"title": "Business Development", "url": "/"},
+                {"title": "Project Member", "url": "/"}
+            ]
+        }
+    ]
+
+    # TODO fix all the links in footer_items to actually point to a page
+    return render_template("index.htm", links=links, footer_items=footer_items)
 
 
 @main.route('/projects')
