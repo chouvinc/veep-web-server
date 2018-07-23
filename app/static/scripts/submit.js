87d7475385e0e9dbbba6eec564ce74a6530494a6
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-07-22 20:49:01 -0400

    Submit form now shows the correct subsection depending on select type.

--- app/static/scripts/submit.js
+++ app/static/scripts/submit.js
@@ -1,2 +1,13 @@
-console.log("Hello world");
-console.log($('.submit_type'));
+$('p[class="submit_type"] > select').change((e) => {
+    showSelectedForms(e.currentTarget.value);
+});
+
+function showSelectedForms (selectedValue) {
+    $('#selections').children().each((index, element) => {
+        if ($(element).attr('id') === selectedValue) {
+            $(element).show();
+        } else {
+            $(element).hide();
+        }
+    });
+}
