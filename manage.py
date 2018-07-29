331488f22f367ed87f3cc9c3cfacd9e458b5298f
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-07-29 17:38:09 -0400

    removed bug

--- manage.py
+++ manage.py
@@ -6,10 +6,5 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-@manager.add_command
-def test():
-    # TODO: add tests LUL
-    pass
-
 if __name__ == '__main__':
     manager.run()
